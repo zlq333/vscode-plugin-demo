6453dca29182329ca6a82c97f1d355c7696050bd
lunqiang.zhou <zhoulq@sijibao.com>
2021-12-15 17:00:10 +0800

    代码优化

--- src/hover.ts
+++ src/hover.ts
@@ -1,18 +1,15 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 module.exports = function (context: vscode.ExtensionContext) {
-	const disposable = vscode.languages.registerHoverProvider('typescript', {
-		provideHover(document, position, token) {
-			console.log('document', document);
-			console.log('position', position);
-			console.log('token', token);
-			const fileName = document.fileName;
-			const word = document.getText(document.getWordRangeAtPosition(position));
-			if (/\.ts$/.test(fileName) && /\bconst\b/.test(word)) {
-				return new vscode.Hover('测试悬停提示-------' + word);
-			}
-		},
-	});
+  const disposable = vscode.languages.registerHoverProvider("typescript", {
+    provideHover(document, position, token) {
+      const fileName = document.fileName;
+      const word = document.getText(document.getWordRangeAtPosition(position));
+      if (/\.ts$/.test(fileName) && /\bconst\b/.test(word)) {
+        return new vscode.Hover("测试悬停提示-------" + word);
+      }
+    },
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 };
